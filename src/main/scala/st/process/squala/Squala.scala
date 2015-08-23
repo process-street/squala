0b8e7614c5c813a51383f5aefbdca5c26b37cc6b
Cameron McKay <me@cdmckay.org>
2015-08-23 13:13:56 -0500

    Add preliminary where clause with equals, and and or

--- src/main/scala/st/process/squala/Squala.scala
+++ src/main/scala/st/process/squala/Squala.scala
@@ -2,24 +2,75 @@ package st.process.squala
 
 // Loosely following this BNF: http://savage.net.au/SQL/sql-92.bnf.html
 
-case class QuerySpec(selectList: SelectList, fromClause: FromClause) {
+case class Select(columnNames: Seq[String]) {
 
-    override def toString = {
-        "select " + selectList.columnNames.mkString(", ") + " from " + fromClause.tableName
-    }
+    def from(tableName: String) = From(Select(columnNames), tableName, optionalAlias = None)
 
-}
-
-case class SelectList(columnNames: Seq[String]) {
-
-    def from(tableName: String) = QuerySpec(this, FromClause(tableName))
+    override def toString = "select " + columnNames.mkString(", ")
 
 }
 
-case class FromClause(tableName: String)
+case class From(selectList: Select, tableName: String, optionalAlias: Option[String]) {
+
+    def as(alias: String) = copy(optionalAlias = Some(alias))
+    
+    def where(searchCondition: SearchCond) = Where(this, searchCondition)
+
+    override def toString =
+        List(Some(selectList.toString), Some("from"), Some(tableName), optionalAlias)
+        .flatten.mkString(" ")
+
+}
+
+case class Where(from: From, searchCondition: SearchCond) {
+
+    override def toString = List(from.toString, "where " + searchCondition.toString).mkString(" ")
+
+}
+
+abstract class SearchCond {
+
+    def ===(that: SearchCond) = EqualsOp(this, that)
+
+    def and(that: SearchCond) = AndOp(this, that)
+
+    def or(that: SearchCond) = OrOp(this, that)
+
+}
+
+case class EqualsOp(lhs: SearchCond, rhs: SearchCond) extends SearchCond {
+    override def toString = s"($lhs = $rhs)"
+}
+
+case class AndOp(lhs: SearchCond, rhs: SearchCond) extends SearchCond {
+    override def toString = s"($lhs and $rhs)"
+}
+
+case class OrOp(lhs: SearchCond, rhs: SearchCond) extends SearchCond {
+    override def toString = s"($lhs or $rhs)"
+}
+
+case class RefExpr(columnName: String) extends SearchCond {
+    override def toString = columnName
+}
+
+case class StrExpr(value: String) extends SearchCond {
+    override def toString = Squala.quote(value)
+}
+
+case class IntExpr(value: Int) extends SearchCond {
+    override def toString = value.toString
+}
 
 object Squala {
 
-    def select(columnNames: String*) = SelectList(columnNames)
+    implicit def symbolToRefExpr(columnName: Symbol): RefExpr = RefExpr(columnName.name)
+    implicit def stringToStrExpr(value: String): StrExpr = StrExpr(value)
+    implicit def intToIntExpr(value: Int): IntExpr = IntExpr(value)
+
+    def select(columnNames: String*) = Select(columnNames)
+
+    def quote(value: String) = "'%s'".format(escape(value))
+    def escape(value: String) = value.replaceAll("'", "''")
 
 }
--- src/test/scala/st/process/SqualaSpec.scala
+++ src/test/scala/st/process/SqualaSpec.scala
@@ -1,16 +1,28 @@
 package st.process
 
 import org.scalatest._
-import st.process.squala.Squala
+import st.process.squala.Squala._
 
 class SqualaSpec extends FlatSpec with Matchers {
 
     "a select * from foo" should "generate corresponding SQL" in {
-        Squala.select("*").from("foo").toString should be ("select * from foo")
+        select("*").from("foo").toString should
+            be ("select * from foo")
     }
 
     "a select x, y, z from foo" should "generate corresponding SQL" in {
-        Squala.select("x", "y", "z").from("foo").toString should be ("select x, y, z from foo")
+        select("x", "y", "z").from("foo").toString should
+            be ("select x, y, z from foo")
+    }
+
+    "a select x, y, z from foo f" should "generate corresponding SQL" in {
+        select("x", "y", "z").from("foo").as("f").toString should
+            be ("select x, y, z from foo f")
+    }
+
+    "a select * from foo where bar = 42" should "generate corresponding SQL" in {
+        (select ("*") from "foo" where((Symbol("x.y.z") === "bar") and ('baz === 44))).toString should
+            be ("select * from foo where ((x.y.z = 'bar') and (baz = 44))")
     }
 
 }
